97be823591161e10eac2e44ca860b752e8e33ba6
sksahmed <smarokadn@gmail.com>
2025-01-07 21:29:56 +0600

    Initial commit for ci_cd_demo

---
+++ ci_cd_demo-main/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Hello, from devops project. version 5!!!!!
+        </p>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/mohammad-akif2000/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn More about Me!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
